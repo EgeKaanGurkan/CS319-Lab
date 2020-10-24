9bcc5339b6800a23595665d68bc64e38704001bc
Ege Kaan Gürkan <ege.gurkan2608@gmail.com>
2020-10-24 13:41:55 +0300

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import PrimeGenerator.java;
+
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        PrimeGenerator primeGenerator = ​new​ PrimeGenerator();
+        System.out.println(primeGenerator.generatePrimes(50));
     }
 }
